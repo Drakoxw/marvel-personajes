0d9a22592ed05a660fb0947e74853d37c6c539aa
Drakoxw <desarrollo13@aveonline.co>
2023-05-12 03:45:38 -0500

    feat: cards ok}

--- components/card-personaje.tsx
+++ components/card-personaje.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function CardPersonaje({ data }:{ data : CardData}) {
   console.log('data :', data.img)
-  const urlOrBg = data.img.includes('image_not_available') ? 'bg-red-800' : `bg-[url('/img/wallpaper.jpg')]`
+  const urlOrBg = data.img.includes('image_not_available') ? 'bg-red-800' : ''
   return (
     <div className="flex">
       <section className="border-gray-500 border rounded">
@@ -15,8 +15,11 @@ export default function CardPersonaje({ data }:{ data : CardData}) {
           {data.name}
         </div>
 
-        <div className={`absolut rounded-full h-[114px] w-[114px] mt-[15px] -ml-[158px] `}>
-          {urlOrBg}
+        <div className={`absolut rounded-full h-[114px] w-[114px] mt-[15px] -ml-[158px] ${urlOrBg}`}>
+          {data.img.includes('image_not_available') 
+            ? <></> 
+            : <img src={data.img} className="rounded-full h-[114px] w-[114px]" />
+          }
         </div>
 
         <div className="absolut bg-[#1d2329] h-8 w-12 mt-[46px] -ml-24">
