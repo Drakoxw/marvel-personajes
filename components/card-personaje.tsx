c26cd1d109f895c0f2733d1b0b2f2757dc6b8307
Drakoxw <desarrollo13@aveonline.co>
2023-05-12 02:47:39 -0500

    feat: card parte 1

--- components/card-personaje.tsx
+++ components/card-personaje.tsx
@@ -2,12 +2,31 @@ import { CardData } from "@/domain/interfaces";
 import Image from "next/image";
 
 export default function CardPersonaje({ data }:{ data : CardData}) {
-
+  console.log('data :', data.img)
   return (
     <div className="flex">
       <section className="border-gray-500 border rounded">
         <Image className="rounded" src={'/img/bg-card.png'} width={200} height={364} alt="personaja marvel" />
       </section>
+      <div className="relative">
+
+        <div className="absolut w-24 h-12 text-center mt-[22px] -ml-[148px]">
+          {data.name}
+        </div>
+
+        <div className={`absolut rounded-full h-[114px] w-[114px] mt-[15px] -ml-[158px] `}>
+          wwwww
+        </div>
+
+        <div className="absolut bg-[#1d2329] h-8 w-12 mt-[46px] -ml-24">
+          {data.comics} {data.img}
+        </div>
+        
+        <div className="absolut bg-[#1d2329] h-8 w-12 mt-[26px] -ml-24">
+          {data.series}
+        </div> 
+
+      </div>
     </div>
   )
 }
